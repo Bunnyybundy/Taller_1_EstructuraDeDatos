95bbdc27a54551025a7f891e53f92025c1f8e4d5
Bunnyybundy <cferap14@gmail.com>
2026-09-19 10:24:14 -0300

    creacion nodo servicio, implementacion de constructor

--- Hospital.cpp
+++ Hospital.cpp
@@ -1,11 +1,37 @@
 
 #include "Hospital.h"
+#include <iostream>
+using namespace std;
 
 Hospital::Hospital() : servicios(nullptr)
 {
-    String nombres[] = {
+    string nombres[] = {
         "Urgencias","Medicina General","Cardiologia",
         "Neurologia","Traumatologia","Cirugia",
         "Pediatria","Hospitalizacion"
     };
+
+    NodoServicio* ultimo = nullptr;
+    for (int i = 0; i < 8; i++) {
+        NodoServicio* nuevo = new NodoServicio(nombres[i]);
+        if (!servicios)
+        {
+            servicios = nuevo;
+            ultimo = nuevo;
+        }else{
+            ultimo->siguiente = nuevo;
+            ultimo = nuevo;
+        }
+    }
+}
+
+void Hospital::mostrarServicios()
+{
+    NodoServicio* actual = servicios;
+    int i = 1;
+    while (actual)
+    {
+        cout << i << "." << actual->nombre << endl;
+        actual = actual->siguiente;
+    }
 }
--- Hospital.h
+++ Hospital.h
@@ -1,11 +1,32 @@
 
 #ifndef TALLER_1_ESTRUCTURADEDATOS_HOSPITAL_H
 #define TALLER_1_ESTRUCTURADEDATOS_HOSPITAL_H
+#include <iostream>
+#include "NodoPaciente.h"
+using namespace std;
 
+struct NodoServicio
+{
+    string nombre;
+    NodoPaciente* paciente;
+    NodoServicio* siguiente;
+
+    NodoServicio(string n): nombre(n), paciente(nullptr), siguiente(nullptr) {}
+};
 
 class Hospital
 {
+private:
+    NodoServicio* servicios;
+
+public:
+    Hospital();
+    ~Hospital();
+    void agregarPacienteAServicio(Paciente* p);
+    void mostrarServicios();
+    void mostrarPacientesPorServicio(string nombreServicio);
 };
+
 
 
 #endif //TALLER_1_ESTRUCTURADEDATOS_HOSPITAL_H
