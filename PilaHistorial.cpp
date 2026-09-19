3ed8b691c162fcd75bdd8a9a52e7062856eab7d9
Bunnyybundy <cferap14@gmail.com>
2026-09-19 10:25:29 -0300

    implementacion pila con push y resto de metodos

--- PilaHistorial.cpp
+++ PilaHistorial.cpp
@@ -1,3 +1,46 @@
 
 
 #include "PilaHistorial.h"
+
+PilaHistorial::~PilaHistorial()
+{
+    while (cima)
+    {
+        NodoHistorial* temp = cima;
+        cima = cima->siguiente;
+        delete temp;
+    }
+}
+
+void PilaHistorial::push(Paciente* p)
+{
+    NodoHistorial* nuevo = new NodoHistorial(p);
+    nuevo->siguiente = cima;
+    cima = nuevo;
+}
+
+Paciente* PilaHistorial::pop()
+{
+    if (!cima) return nullptr;
+    NodoHistorial* temp = cima;
+    Paciente* p = temp->paciente;
+    cima = cima->siguiente;
+    delete temp;
+    return p;
+}
+
+void PilaHistorial::mostrar()
+{
+    cout << " === HISTORIAL DE ATENCIONES === " << endl;
+    NodoHistorial* actual = cima;
+    if (!actual)
+    {
+        cout << "No hay atenciones registradas." << endl;
+        return;
+    }while (actual)
+    {
+        actual->paciente->mostrarInfo();
+        cout << endl;
+        actual = actual->siguiente;
+    }
+}
--- PilaHistorial.h
+++ PilaHistorial.h
@@ -2,9 +2,29 @@
 #ifndef TALLER_1_ESTRUCTURADEDATOS_PILAHISTORIAL_H
 #define TALLER_1_ESTRUCTURADEDATOS_PILAHISTORIAL_H
 
+#include "Paciente.h"
+#include <iostream>
+using namespace std;
+
+struct NodoHistorial
+{
+  Paciente* paciente;
+  NodoHistorial* siguiente;
+  NodoHistorial(Paciente* p) : paciente(p), siguiente(nullptr){}
+};
 
 class PilaHistorial
 {
+  private:
+   NodoHistorial* cima;
+  public:
+    PilaHistorial():cima(nullptr){}
+    ~PilaHistorial();
+
+    void push(Paciente* p);
+    Paciente* pop();
+    void mostrar();
+    bool estaVacia() const { return  cima == nullptr; }
 };
 
 
