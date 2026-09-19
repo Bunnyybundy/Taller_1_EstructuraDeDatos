aba9a666e3964287d2fcedfe57f023d4be66a42d
Bunnyybundy <cferap14@gmail.com>
2026-09-19 10:24:58 -0300

    implementacion destructor y definincion de metodos enqueue, dequeue

--- ColaPacientes.cpp
+++ ColaPacientes.cpp
@@ -1,3 +1,52 @@
 
 
+
 #include "ColaPacientes.h"
+#include <iostream>
+using namespace std;
+
+ColaPacientes::~ColaPacientes()
+{
+    while (frente)
+    {
+        NodoPaciente* temp = frente;
+        frente = frente->siguiente;
+        delete temp ->paciente;
+        delete temp;
+    }
+}
+
+void ColaPacientes::enqueue(Paciente* p )
+{
+    NodoPaciente* nuevo = new NodoPaciente(p);
+    if (!frente)
+    {
+        frente = fin = nuevo;
+    }else {
+        fin->siguiente = nuevo;
+        fin = nuevo;
+    }
+}
+Paciente* ColaPacientes::dequeue()
+{
+    if (!frente)return nullptr;
+    NodoPaciente* temp = frente;
+    Paciente* p = temp->paciente;
+    frente = frente->siguiente;
+    if (!frente) fin = nullptr;
+    delete temp;
+    return p;
+}
+
+void ColaPacientes::mostrar()
+{
+    NodoPaciente* actual = frente;
+    int i = 1;
+    while (actual)
+    {
+        cout << i++ << ". ";
+        actual->paciente->mostrarInfo();
+        cout << endl;
+        actual = actual->siguiente;
+    }
+}
--- ColaPacientes.h
+++ ColaPacientes.h
@@ -2,10 +2,25 @@
 #ifndef TALLER_1_ESTRUCTURADEDATOS_COLAPACIENTES_H
 #define TALLER_1_ESTRUCTURADEDATOS_COLAPACIENTES_H
 
+#include "NodoPaciente.h"
+#include <iostream>
+#include "Paciente.h"
+using namespace std;
 
 class ColaPacientes
 {
-};
+private:
+    NodoPaciente* frente;
+    NodoPaciente* fin;
 
+public:
+    ColaPacientes():frente(nullptr),fin(nullptr){}
+    ~ColaPacientes();
+
+    void enqueue(Paciente* p );
+    Paciente* dequeue();
+    void mostrar();
+    bool estaVacia() const{ return frente == nullptr; };
+};
 
 #endif //TALLER_1_ESTRUCTURADEDATOS_COLAPACIENTES_H
